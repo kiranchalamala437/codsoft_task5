f16e933a1223677bbe7b9c2dafddfc7c18158f61
kiranchalamala437 <153431096+kiranchalamala437@users.noreply.github.com>
2023-12-12 10:45:02 +0530

    task5

---
+++ contact_book.py
@@ -0,0 +1,111 @@
+#importing tkinter library
+from tkinter import *
+from tkinter import messagebox
+
+#Initialize window
+root = Tk()
+root.geometry("700x550")
+root.config(bg="#d3f3f5")
+root.title("Contact Book")
+root.resizable(0,0)
+contactlist = []
+
+Name = StringVar()
+Number = StringVar()
+
+frame = Frame(root)
+frame.pack(side= RIGHT)
+
+scroll =Scrollbar(frame,orient=VERTICAL)
+select = Listbox(frame, yscrollcommand=scroll.set, font=("Times new roman", 16), 
+        bg="#f0fffc", width=20, height=20, borderwidth=3, relief= "groove")
+scroll.config(command=select.yview)
+scroll.pack(side=RIGHT, fill=Y)
+select.pack(side=LEFT, fill=BOTH,expand=1)
+
+#Function to select a contact
+def Selected():
+    print("hello", len(select.curselection()))
+    if len(select.curselection())==0:
+        messagebox.showerror("Error", "Please select the Name")
+    else:
+        return int(select.curselection()[0])
+
+#Function to Adding new contact to ContactList
+def AddContact():
+    if Name.get() !="" and Number.get() !="":
+        contactlist.append([Name.get(), Number.get()])
+        print(contactlist)
+        Select_set()
+        EntryReset()
+        messagebox.showinfo("Confirmation", "Successfully Added New Contact")
+    else:
+        messagebox.showerror("Error","Please fill the information")
+
+#Function to update the details
+def UpdateDetail():
+    if Name.get() and Number.get():
+        contactlist[Selected()] = [Name.get(),Number.get()]
+        print(contactlist)
+        messagebox.showinfo("Confirmation", "Successfully Updated Contact")
+        EntryReset()
+        Select_set()
+
+    elif not(Name.get()) and not(Number.get()) and not(len(select.curselection())==0):
+        messagebox.showerror("Error", "Please fill the information")
+
+    else:
+        if len(select.curselection())==0:
+            messagebox.showerror("Error", "Please Select the name and \n press Load button")
+        else:
+            message1 = """To Load the all information of \n
+                selected row press Load button \n.""" 
+            messagebox.showerror("Error", message1)
+
+#Function to Delete an Entry from the ContactList
+def DeleteEntry():
+    if len(select.curselection()) != 0:
+        result = messagebox.askyesno("confirmation", "You want to Delete Contact \n Which you selected")
+        if result == True:
+            del contactlist[Selected()]
+            Select_set()
+    else:
+        messagebox.showerror("Error", "Please select the Contact")
+
+#Viewing the selected contact
+def VIEW():
+    NAME, PHONE= contactlist[Selected()]
+    Name.set(NAME)
+    Number.set(PHONE)
+
+#Exiting the Game Window
+def EXIT():
+    root.destroy()
+
+#selecting a contact
+def Select_set():
+    contactlist.sort()
+    select.delete(0,END)
+    for name,phone in contactlist:
+        select.insert(END,name)
+Select_set()
+
+#Reset the entry
+def EntryReset():
+    Name.set("")
+    Number.set("")
+
+#define buttons labels and entry widget
+Label(root, text = 'Name', font=("Times new roman",22,"bold"), bg = 'SlateGray3').place(x= 30, y=20)
+Entry(root, textvariable = Name, width=30).place(x= 200, y=30)
+Label(root, text = 'Contact No.', font=("Times new roman",20,"bold"),bg = 'SlateGray3').place(x= 30, y=70)
+Entry(root, textvariable = Number, width=30).place(x= 200, y=80)
+
+Button(root,text=" ADD", font='Helvetica 18 bold',bg='#e8c1c7', command = AddContact, padx=20). place(x= 50, y=140)
+Button(root,text="EDIT", font='Helvetica 18 bold',bg='#e8c1c7',command = UpdateDetail, padx=20).place(x= 50, y=200)
+Button(root,text="DELETE", font='Helvetica 18 bold',bg='#e8c1c7',command = DeleteEntry, padx=20).place(x= 50, y=260)
+Button(root,text="VIEW", font='Helvetica 18 bold',bg='#e8c1c7', command = VIEW).place(x= 50, y=325)
+Button(root,text="RESET", font='Helvetica 18 bold',bg='#e8c1c7', command = EntryReset).place(x= 50, y=390)
+Button(root,text="EXIT", font='Helvetica 24 bold',bg='tomato', command = EXIT).place(x= 250, y=470)
+ 
+root.mainloop()
